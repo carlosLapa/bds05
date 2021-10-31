afbc37a8003baf4a5a14cc359ef68aa91a6a5b87
carlosLapa <carlosferreiralapa@gmail.com>
2021-10-31 12:58:56 +0000

    All tests passing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,12 +5,11 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_VISITOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 
 INSERT INTO tb_genre(name) VALUES ('Aventura');
 
 INSERT INTO tb_movie (title, sub_Title, year, img_Url, synopsis, genre_id) VALUES ('Gladiador', 'The hope of an Empire', 2000, 'https://pt.wikipedia.org/wiki/Gladiador_(filme)#/media/Ficheiro:GladiadorPoster.jpg', 'Um gladiador desafia um Império', 1);
 
-INSERT INTO tb_review (text, user_id, movie_id) VALUES ('Filme muito bom!', 1, 1);
-INSERT INTO tb_review (text, user_id, movie_id) VALUES ('Excelente filme e uma banda sonora de arrebatar!', 1, 1);
+INSERT INTO tb_review (text, movie_id, user_id) VALUES ('Filme muito bom!', 1, 1);
+INSERT INTO tb_review (text, movie_id, user_id) VALUES ('Excelente filme e uma banda sonora de arrebatar!', 1, 2);
